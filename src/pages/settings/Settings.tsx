1f01cd14cf3eca96997fb3b34eb396e56d4898f7
bearlifter <tavauditore@gmail.com>
2023-06-08 15:15:15 -0700

    implemente lo necesario para hacer la primera transaccion

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,3 +1,10 @@
+import { TransferButton } from "Transfer (3)";
+import { TotalSupply } from "TotalSupply";
+
 export default function Settings() {
-  return <div>settings</div>;
+  return <div>
+    
+    <TransferButton to = "12" amount={5}></TransferButton>
+    <TotalSupply></TotalSupply>
+  </div>;
 }
